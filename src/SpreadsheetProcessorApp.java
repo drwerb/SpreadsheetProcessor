398733e9a5849d35895df957779e5e25a8dbff5b
Vova <drwerb@yahoo.com>
2016-03-02 18:07:23 +0200

    application class renamed

--- src/SpreadsheetProcessorApp.java
+++ src/SpreadsheetProcessorApp.java
@@ -1,8 +1,8 @@
-class TableProcessorApp {
+class SpreadsheetProcessorApp {
     public static void main(String[] args) {
         Spreadsheet spreadsheet = new Spreadsheet4Types();
 
-        new SpreadsheetReaderFile("src/14_expression_with_negative_ref.table").readAndFillSpreadsheet(spreadsheet);
+        (new SpreadsheetReaderStdin()).readAndFillSpreadsheet(spreadsheet);
 
         spreadsheet.processData();
 
